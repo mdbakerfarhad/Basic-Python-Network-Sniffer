ef0b6822f199825d245ce2689dba9bdd8cfdcc53
Md. Baker <mdbakerfarhad@gmail.com>
2025-03-02 22:09:06 +0600

    added suppress warnings

--- Basic_Network_Sniffer.py
+++ Basic_Network_Sniffer.py
@@ -3,3 +3,17 @@ from datetime import datetime
 import subprocess
 import sys
 import signal
+
+# Suppress Scapy warnings
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+logging.getLogger("scapy.interactive").setLevel(logging.ERROR)
+logging.getLogger("scapy.loading").setLevel(logging.ERROR)
+
+try:
+    from scapy.all import *
+except ImportError:
+    print("Scapy package for Python is not installed on your system.")
+    sys.exit()
+
+# Print a message to the user
+print("\n! Make sure to run this program as ROOT !\n")
